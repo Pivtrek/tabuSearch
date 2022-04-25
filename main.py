8ad1ebca0d9cadf0442b530c9390b5305671be04
Pivtrek <piotrek.bijacz.7@gmail.com>
2022-04-25 14:18:14 +0200

    added neighbourhood function to generateNeighbours.py

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
 from generateNeighbours import GenerateGraph
-from tabuSearch import tabu
 
 graph = GenerateGraph('FULL_MATRIX', 5, 50)
-dist, path = graph.two_opt()
-iterations = 50
-maxSizeTabu = 100
-print(graph)
-print(dist)
-print(path)
-tabu(graph, dist, path, iterations, maxSizeTabu)
+GenerateGraph.neighbourhood(graph)
+
